ab49268ed3ea22feccbd24a505db6eb5a5b6df1e
rock <yazin@whitepayments.com>
2015-07-24 14:09:38 +0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Set configuration options
+requirejs.config({
+  baseUrl: 'js',
+  paths: {
+    jquery: [
+      '//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min',
+      'lib/jquery-1.11.3.min' ],
+  },
+});
